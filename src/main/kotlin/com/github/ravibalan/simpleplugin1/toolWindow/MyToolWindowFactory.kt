7127ae72b7ae898ce211bf1097172ff0d7f0410e
ravi <ravikbalan@gmail.com>
2023-07-19 06:44:59 -0400

    Add a open webpage button

--- src/main/kotlin/com/github/ravibalan/simpleplugin1/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/ravibalan/simpleplugin1/toolWindow/MyToolWindowFactory.kt
@@ -1,5 +1,7 @@
 package com.github.ravibalan.simpleplugin1.toolWindow
 
+import com.github.ravibalan.simpleplugin1.MyBundle
+import com.github.ravibalan.simpleplugin1.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
@@ -8,9 +10,11 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import com.github.ravibalan.simpleplugin1.MyBundle
-import com.github.ravibalan.simpleplugin1.services.MyProjectService
+import java.awt.Desktop
+import java.awt.FlowLayout
+import java.net.URI
 import javax.swing.JButton
+import javax.swing.JPanel
 
 
 class MyToolWindowFactory : ToolWindowFactory {
@@ -21,8 +25,30 @@ class MyToolWindowFactory : ToolWindowFactory {
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val myToolWindow = MyToolWindow(toolWindow)
-        val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), null, false)
-        toolWindow.contentManager.addContent(content)
+        val content = ContentFactory.getInstance().createContent(myToolWindow.getContent(), "JSON-Visualizer", false)
+
+        // Create the content panel for the custom tool window
+        val panel = JPanel()
+        panel.layout = FlowLayout()
+
+        // Create the JButton for opening the webpage
+        val openWebpageButton = JButton("Open JSON VIsualizer")
+
+        // Add an ActionListener to the button to open the webpage
+        openWebpageButton.addActionListener {
+            try {
+                Desktop.getDesktop().browse(URI("https://www.google.com"))
+            } catch (ex: Exception) {
+                ex.printStackTrace()
+            }
+        }
+
+        // Add the button to the panel
+        panel.add(openWebpageButton)
+
+        val content2 = ContentFactory.getInstance().createContent(panel ,"JSON-Visualizer", false)
+        toolWindow.contentManager.addContent(content2)
+
     }
 
     override fun shouldBeAvailable(project: Project) = true
